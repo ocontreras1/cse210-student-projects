a65465f776d37e786eb628c864020e3781cd732d
ocontreras1 <ocontrerascruz@byupathway.edu>
2025-07-05 08:57:37 -0400

    Exercise 3 from week 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Create new Random class and a random number generator
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 20);
+        int guessNumber = 0;
+
+        // do while loop to keep asking for a number until user guesses the magic number
+        do
+        {
+            Console.Write("What is the magic number: ");
+            string guess = Console.ReadLine();
+
+            guessNumber = int.Parse(guess);
+
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        while (guessNumber != magicNumber);
     }
 }
