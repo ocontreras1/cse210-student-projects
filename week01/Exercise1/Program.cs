2263470e39a63a8d86d432d4b69fe8fce14cdc20
ocontreras1 <ocontrerascruz@byupathway.edu>
2025-07-05 01:25:47 -0400

    Excercise 1 from week 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Get user imput for name and last name and store in variables
+        Console.WriteLine("What is your first name?");
+        string first_name = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string last_name = Console.ReadLine();
+
+        // Display the name in last name, firs name format
+        Console.WriteLine($"Your name is {last_name}, {first_name}");
     }
 }
