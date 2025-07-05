faa4530847a5b3f4fd61a33cbf071f82754c3ab1
ocontreras1 <ocontrerascruz@byupathway.edu>
2025-07-05 11:08:28 -0400

    Exercise 5 from week 1

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,56 @@ using System;
 
 class Program
 {
+    
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        // Get name and number from user by using functions
+        string userName = PromptUserName();
+
+        int userNumber = PromptUserNumber();
+
+        // square the number
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
+
     }
+
+    // Function to display a welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Function to get a name from user
+    static string PromptUserName()
+    {
+        Console.Write("What is your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    // Function to get number from user
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favourite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    // Function to square the number
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    // Function to display the name and the square of the number
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+    
 }
